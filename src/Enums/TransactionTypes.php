d5020a785e37d634be404057f34f155f97c10c85
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2022-05-14 13:48:37 +0200

    update transactions enum

--- src/Enums/TransactionTypes.php
+++ src/Enums/TransactionTypes.php
@@ -5,8 +5,26 @@ namespace Payir\SDK\Enums;
 class TransactionTypes
 {
     const DEPOSIT = "1";
+
     const TRANSFER = "2";
+
     const CASHOUT = "3";
+
     const GATEWAY = "4";
+
+    const SMS_DEPOSIT = "6";
+
+    const REFUND = "7";
+
+    const OFFLINE_DEPOSIT = "10";
+
+    const CREATE_WALLET = "12";
+
     const PERSONAL_PAYMENT = "13";
+
+    /**
+     * not supported anymore
+     * @deprecated
+     */
+    const EZ_FORM = "15";
 }
